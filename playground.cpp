d6f7068101867ce8f7f3db5d471da87492487920
Mohammed Amarnah <m.amarnah@gmail.com>
2019-06-24 19:01:15 +0300

    remove unused code from playground.cpp

--- playground.cpp
+++ playground.cpp
@@ -5,8 +5,6 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
-#define TAKE_INPUT(v, n) for(int i = 0; i < n; ++i) scanf("%d", &v[i])
-#define OUTPUT(v, n) for(int i = 0; i < n; ++i) cout << v[i] << " "; cout << endl;
 
 int main() {
 #ifndef ONLINE_JUDGE
